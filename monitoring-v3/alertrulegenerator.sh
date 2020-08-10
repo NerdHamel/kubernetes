2fe735d6ad89a4c40df09a7f7a0438be6267e473
tanzeem <tanzeemzillu@gmail.com>
2020-08-10 10:19:56 +0200

    add the grafana templates

--- monitoring-v3/alertrulegenerator.sh
+++ monitoring-v3/alertrulegenerator.sh
@@ -2,15 +2,21 @@
 read -p "Enter HOST: " host
 read -p "Enter pager duty integration api key:" apikey
 read -p "Enter all the IP which you want to use as Node-exporter target. The input format will be like 'IP-1:31000', 'IP-2:31000',..,'IP-n:31000':" IP
+read -p "Enter the endpoint url: " endpoint
+read -p "Enter the api url: " api
 host=$host
 apikey=$apikey
-IP=$IP 
+IP=$IP
+endpoint=$endpoint
+api=$api
 
 
 sed -i "s/<Host>/$host/g" prometheus/configmaps/alertrule.yaml
 sed -i "s/<node-exporter-ip>/$IP/g" prometheus/configmaps/config-map.yaml
 sed -i "s/<Host>/$host/g" alert-manager/configmap.yaml
 sed -i "s/<api-key>/$apikey/g" alert-manager/configmap.yaml
+sed -i "s/<endpoint-url>/$endpoint/g" grafana/configmaps/dashboard.yaml
+sed -i "s/<api-url>/$api/g" grafana/configmaps/dashboard.yaml
 
 # cp alertrule.yaml configmaps/alertrule.yaml
 if [ "$host" == "localhost" ]
