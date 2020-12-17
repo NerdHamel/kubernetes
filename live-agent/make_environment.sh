df34f863ece77a7b21ea00cf400d2bdda493502e
Benjamin Mayr <b.mayr@cognigy.com>
2020-12-17 11:39:52 +0100

    Add 'make_environment' script for live-agent

---
+++ live-agent/make_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -d "$1" ]; then
+    echo "Creating a copy of the 'template.dist' folder for your stage..."
+    cp -R template.dist $1
+fi
+
+echo "Making the 'manifests' current in your '$1' folder for your stage..."
+rm -fr $1/manifests
+cp -R manifests $1/manifests
